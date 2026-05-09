6b9ffb15c099c607c90be9361c33dbc589ec588a
yoppei0818 <yoppei0818@gmail.com>
2026-05-09 22:26:49 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: '2024-04-03',
+})
