e5a7a51f793ff1bf2ce8381eb50dcffb8f8bd526
yoppei0818 <yoppei0818@gmail.com>
2026-05-09 22:35:28 +0900

    feat: tailwindcssの導入

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,6 @@
 <template>
   <NuxtRouteAnnouncer />
-  <NuxtPage />
+  <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
+    '@nuxt/ui',
   ],
+  css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
 })
